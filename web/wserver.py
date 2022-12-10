10fa497962549d5bb4111bc7c40048b2e94c6957
beast06king <116450986+beast06king@users.noreply.github.com>
2022-12-10 20:09:43 +0530

    Update wserver.py

--- web/wserver.py
+++ web/wserver.py
@@ -624,8 +624,8 @@ section span{
         </a>
       </div>
       <div class="social">
-        <a href="https://github.com/weebzone/WZML"><i class="fab fa-github"></i></a>
-        <a href="https://t.me/krn2701"><i class="fab fa-telegram"></i></a>
+        <a href="https://github.com/beast06king/MirBot"><i class="fab fa-github"></i></a>
+        <a href="https://t.me/crazy_phoenix"><i class="fab fa-telegram"></i></a>
       </div>
     </header>
     <section>
@@ -772,7 +772,7 @@ def set_priority(id_):
 
 @app.route('/')
 def homepage():
-    return "<h1>See mirror-with-weeb <a href='https://github.com/weebzone/WZML'>@GitHub</a> By <a href='https://github.com/weebzone'>Code With Weeb</a></h1>"
+    return "<h1>See mirror-with-weeb <a href='https://github.com/beast06king/MirBot'>@GitHub</a> By <a href='https://github.com/beast06phoenix'>Code With Weeb</a></h1>"
 
 @app.errorhandler(Exception)
 def page_not_found(e):
